34f9d47265ea9479f2a9635930685b2846587628
rowshon <rowshon.chowdhoury@bracits.com>
2024-01-19 19:12:58 +0600

    init

---
+++ src/main/java/com/example/rc/DemoRabitMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRabitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRabitMqApplication.class, args);
+	}
+
+}
